5cbe08a4918344f6c72169e557ec4b0afe2462e5
Ayush Deb <ayushdeb848@gmail.com>
2025-02-03 12:50:51 +0530

    removed modules

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { AudioUploader } from './components/AudioUploader';
+//import { AudioUploader } from './components/AudioUploader';
 import { Hero } from './components/Hero';
 import { Navbar } from './components/Navbar';
 
 const App: React.FC = () => {
-  const handleFileUpload = (file: File) => {
+  /*const handleFileUpload = (file: File) => {
     console.log('Uploaded audio file:', file);
     // You can process the file further here (e.g., upload to a server)
-  };
+  };*/
 
   return (
     <div>
